2a5a43c60dd68b7112e8b2b81c397640ff7e56d2
Seii <seii.tanomoshii@gmail.com>
2017-04-03 22:35:34 +0200

    Remove services code

--- src/main/java/com/solarvillage/services/MainServiceActivator.java
+++
@@ -1,9 +0,0 @@
-package com.solarvillage.services;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
- 
-@ApplicationPath("/rest")
-public class MainServiceActivator extends Application {
-
-}
