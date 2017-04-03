2a5327ede8ea7d1b0800a799ae76d1e62aae5ccb
Seii <seii.tanomoshii@gmail.com>
2017-04-03 22:36:58 +0200

    Revert "Remove services code"
    This reverts commit 2a5a43c60dd68b7112e8b2b81c397640ff7e56d2.

---
+++ src/main/java/com/solarvillage/services/MainServiceActivator.java
@@ -0,0 +1,9 @@
+package com.solarvillage.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+ 
+@ApplicationPath("/rest")
+public class MainServiceActivator extends Application {
+
+}
