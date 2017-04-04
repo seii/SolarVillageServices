30195d6e07863dc52d32d788118517347f3e129c
Seii <seii.tanomoshii@gmail.com>
2017-04-04 19:43:22 +0200

    Add query error to data model

---
+++ src/main/java/com/solarvillage/datamodel/GovtStatusError.java
@@ -0,0 +1,20 @@
+package com.solarvillage.datamodel;
+
+public class GovtStatusError {
+	private String queryType;
+	private String queryResponse;
+	
+	public String getQueryType() {
+		return queryType;
+	}
+	public void setQueryType(String queryType) {
+		this.queryType = queryType;
+	}
+	public String getQueryResponse() {
+		return queryResponse;
+	}
+	public void setQueryResponse(String queryResponse) {
+		this.queryResponse = queryResponse;
+	}
+	
+}
